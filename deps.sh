066da94040e266eda6ca0e9c6d8550f21b25e77f
Othi <mnpq.raven@gmail.com>
2024-10-29 14:34:03 +0700

    chore

--- deps.sh
+++ deps.sh
@@ -1,6 +1,19 @@
 # platform: ubuntu 24
 
+cd $HOME
+
 sudo apt upgrade -y
+sudo apt-get install gcc
 
 sudo snap install docker
 sudo snap install btop
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+
+mkdir -p service_repos
+
+# install cmd cli
+git -C $HOME/service_repos clone https://github.com/mnpqraven/vps-do
+cargo install --path $HOME/service_repos/vps-do
