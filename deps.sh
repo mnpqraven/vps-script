200412331870271eb39a8495b4d4306e06f1e07f
Othi <mnpq.raven@gmail.com>
2024-11-08 11:24:24 +0700

    chore

--- deps.sh
+++ deps.sh
@@ -3,10 +3,22 @@
 cd $HOME
 
 sudo apt upgrade -y
+
+export EDITOR=$(which vim)
+
 sudo apt-get install gcc
+
+# netstat
+sudo apt install net-tools
 
 sudo snap install docker
 sudo snap install btop
+
+# open default port for nginx
+iptables -I INPUT 6 -m state --state NEW -p tcp --dport 80 -j ACCEPT
+
+# package manager
+curl -fsSL https://get.pnpm.io/install.sh | sh -
 
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
